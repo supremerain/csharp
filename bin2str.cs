36bf6ba17c5a2b8c5b13dc39d72f2872f3239bd3
supremerain <penghongbo@hotmail.com>
2022-07-21 14:11:55 +0900

    Update bin2str.cs

--- bin2str.cs
+++ bin2str.cs
@@ -11,7 +11,26 @@ class Bin2str {
         
         int readSize;
         int remain = fileSize;
-        int bufpos = 0;
+        int bufPos = 0;
         
+        Console.WriteLine("filesize={0}Byte(s)", fileSize);
+        
+        StringBuffer sb = new StringBuffer();
+        while (remain > 0) {
+            readSize = fs.Read(buf, bufPos, Math.Min(1024, remain));
+            
+            bufPos += readSize;
+            remain -= readSize;
+        }
+        
+        Console.WriteLine("read file complete..", fileSize);
+        fs.Dispose();
+        
+        for(int i = 0; i < buf.Length; i++){
+            sb.Append(buf[i].ToString("x2"));
+        }
+        
+        File.WriteAllText(@args[1], sb.ToString());
+        Console.WriteLine("write file complete.");
     }
 }
